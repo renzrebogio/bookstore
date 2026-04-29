2cb9200f9d320eca30e97d82aadd68ed4866496d
Elijah Boon <elijahboon986@gmail.com>
2026-04-30 00:19:27 +0800

    Delete book-store-admin-page directory

--- book-store-admin-page/component/inventoryHeader/addItemModal.php
+++
@@ -1,6 +0,0 @@
-<div class="modal-container">
-    <div class="modal-header">
-        <h2>Add items to inventory</h2>
-    </div>
-    <form action=""></form>
-</div>
--- book-store-admin-page/component/inventoryHeader/inventoryHeader.php
+++
@@ -1,8 +0,0 @@
-<div class="inventory-header">
-    <div class="page-title">
-        <h1>INVENTORY</h1>
-    </div>
-    <div class="btn-container">
-        <button class="add-btn" id="addBtn"><span class="material-icons-outlined">add_circle</span>ADD <span class="hide-on-mobile">ITEMS</span></button>
-    </div>
-</div>
